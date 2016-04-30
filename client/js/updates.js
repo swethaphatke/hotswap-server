a85f75f135c8bc605db64f6956cc9e7fb06936b9
Vinay Bedre <vinaybedre@gmail.com>
2016-04-30 10:34:28 +0530

    auto update

--- client/js/updates.js
+++ client/js/updates.js
@@ -3,13 +3,43 @@ var fs = require("fs");
 var fse = require("fs-extra");
 var zipper = require('zip-local');
 
-fse.emptyDir('updates', function (err) {
-    request("http://localhost:3000/update/app.zip").pipe(fs.createWriteStream("updates/updated.zip")).on("close", function () {
-        zipper.sync.unzip("updates/updated.zip").save("updates");
-        fs.unlink("updates/updated.zip");
-        fse.copy('updates', 'views', { clobber: true }, function (err) {
-            if (err) return console.error(err)
-            console.log("success!");
+if (!localStorage.getItem('appVersion')) {
+    localStorage.setItem('appVersion', 1);
+}
+
+// var myNotification = new Notification('Updates available', {
+//   body: 'Hold tight, your application will be updated soon.'
+// });
+
+var updatedAppVersion = 0;
+$("#refresh").on("click", function () {
+    var appVersion = localStorage.getItem('appVersion');
+    $.get("http://localhost:3000/version", function (data) {
+        updatedAppVersion = data.version;
+        if (updatedAppVersion > appVersion) {
+            $(".hidden").removeClass("hidden");
+            var myNotification = new Notification('Updates available', {
+                body: 'Hold tight, your application will be updated soon.'
+            });
+            updateApp();
+        }else{
+            console.log("no updates available");
+        }
+    })
+});
+
+function updateApp() {
+    fse.emptyDir('updates', function (err) {
+        request("http://localhost:3000/update/app.zip").pipe(fs.createWriteStream("updates/updated.zip")).on("close", function () {
+            zipper.sync.unzip("updates/updated.zip").save("updates");
+            fs.unlink("updates/updated.zip");
+            fse.copy('updates', 'views', { clobber: true }, function (err) {
+                if (err) return console.error(err)
+                console.log("success!");
+                localStorage.setItem("appVersion", updatedAppVersion);
+                location.reload();
+            });
         });
     });
-});
+}
+
--- server.js
+++ server.js
@@ -6,39 +6,25 @@ var zipper = require("zip-local");
 
 app.use(express.static(__dirname));
 app.get('/', function (req, res) {
-  // res.sendfile(__dirname + '/index.html');
   res.send('Hello World! Vinay');
 });
 
 app.get('/version', function (req, res) {
   res.setHeader('Content-type', 'application/json');
-  res.send('{"version": 2}');
+  res.send('{"version": 1.13}');
 });
 
 app.get('/update/app.zip', function (req, res) {
   var file = __dirname + '/app.zip';
-  
+
   zipper.sync.zip("./client").compress().save("app.zip");
   res.setHeader('Content-disposition', 'attachment; filename=app.zip');
   res.setHeader('Content-type', 'application/zip');
-  
+
   var filestream = fs.createReadStream(file);
   filestream.pipe(res);
-  
+
   fs.unlink("app.zip");
-
-  // var zip5 = new EasyZip();
-  // zip5.zipFolder('client', function () {
-  //   zip5.writeToFile('app.zip');
-  //   var file = __dirname + '/app.zip';
-
-  //   res.setHeader('Content-disposition', 'attachment; filename=app.zip');
-  //   res.setHeader('Content-type', 'application/zip');
-
-  //   var filestream = fs.createReadStream(file);
-  //   filestream.pipe(res);
-  // });
-
 });
 
 module.exports = app;
